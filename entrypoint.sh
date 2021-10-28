46a7c6f594677b453f486cf099e7fdc13f92c8f6
Łukasz Sypniewski <lukasz.sypniewski@guestline.com>
2021-10-28 17:32:53 +0200

    Fix ShellCheck warnings

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# shellcheck disable=SC2153
+
 set -o pipefail
 
 # config
@@ -28,48 +30,48 @@ echo "Preparing Docker tags for repository"
 IMAGE_ID=$domain/$project_name/$image_name
 
 # Change all uppercase to lowercase
-IMAGE_ID=$(echo $IMAGE_ID | tr '[A-Z]' '[a-z]')
+IMAGE_ID=$(echo "$IMAGE_ID" | tr '[:upper:]' '[:lower:]')
 
 # Strip "v" prefix from tag name
-VERSION_FROM_GIT_TAG=$(echo $current_git_tag | sed -e 's/^v//')
+VERSION_FROM_GIT_TAG=$(echo "$current_git_tag" | sed -e 's/^v//')
 
 # Use Docker `latest` tag convention
 LATEST_VERSION="latest"
 
-[ ! -z "$custom_suffix" ]  && TAG_CUSTOM="$IMAGE_ID:${custom_version}-${custom_suffix}"
+[ -n "$custom_suffix" ]  && TAG_CUSTOM="$IMAGE_ID:${custom_version}-${custom_suffix}"
 [ -z "$custom_suffix" ]  && TAG_CUSTOM="$IMAGE_ID:${custom_version}"
 
 TAG_WITH_SUFFIX="$IMAGE_ID:${custom_suffix}"
 
-[ ! -z "$custom_suffix" ]  && TAG_FROM_GIT_TAG="$IMAGE_ID:${VERSION_FROM_GIT_TAG}-${custom_suffix}"
+[ -n "$custom_suffix" ]  && TAG_FROM_GIT_TAG="$IMAGE_ID:${VERSION_FROM_GIT_TAG}-${custom_suffix}"
 [ -z "$custom_suffix" ]  && TAG_FROM_GIT_TAG="$IMAGE_ID:${VERSION_FROM_GIT_TAG}"
 
 TAG_LATEST="$IMAGE_ID:$LATEST_VERSION"
 TAGS_TO_ADD=""
-[ $add_latest_tag == "true" ] && TAGS_TO_ADD="$TAG_LATEST"
-[[ ! -z "$custom_version" ]] && TAGS_TO_ADD="$TAGS_TO_ADD,$TAG_CUSTOM"   
-[[ ! -z "$custom_suffix" ]] && TAGS_TO_ADD="$TAGS_TO_ADD,$TAG_WITH_SUFFIX"          
-[ $use_git_tag == "true" ] && TAGS_TO_ADD="$TAGS_TO_ADD,$TAG_FROM_GIT_TAG"
+[ "$add_latest_tag" == "true" ] && TAGS_TO_ADD="$TAG_LATEST"
+[[ -n "$custom_version" ]] && TAGS_TO_ADD="$TAGS_TO_ADD,$TAG_CUSTOM"   
+[[ -n "$custom_suffix" ]] && TAGS_TO_ADD="$TAGS_TO_ADD,$TAG_WITH_SUFFIX"          
+[ "$use_git_tag" == "true" ] && TAGS_TO_ADD="$TAGS_TO_ADD,$TAG_FROM_GIT_TAG"
 
 # Trim commas
-TAGS_TO_ADD=$(echo $TAGS_TO_ADD  | sed 's/^[,]*//;s/[,]*$//') 
+TAGS_TO_ADD=$(echo "$TAGS_TO_ADD"  | sed 's/^[,]*//;s/[,]*$//') 
 
 # Print created tags
 echo "*** Created variables ***"
-echo IMAGE_ID=$IMAGE_ID
-echo CUSTOM_VERSION=$custom_version
-echo VERSION_FROM_GIT_TAG=$VERSION_FROM_GIT_TAG
+echo IMAGE_ID="$IMAGE_ID"
+echo CUSTOM_VERSION="$custom_version"
+echo VERSION_FROM_GIT_TAG="$VERSION_FROM_GIT_TAG"
 echo LATEST_VERSION=$LATEST_VERSION
-echo TAG_CUSTOM=$TAG_CUSTOM
-echo TAG_FROM_GIT_TAG=$TAG_FROM_GIT_TAG
-echo TAG_LATEST=$TAG_LATEST
-echo TAG_WITH_SUFFIX=$TAG_WITH_SUFFIX
-echo TAGS_TO_ADD:${TAGS_TO_ADD}    
+echo TAG_CUSTOM="$TAG_CUSTOM"
+echo TAG_FROM_GIT_TAG="$TAG_FROM_GIT_TAG"
+echo TAG_LATEST="$TAG_LATEST"
+echo TAG_WITH_SUFFIX="$TAG_WITH_SUFFIX"
+echo TAGS_TO_ADD:"${TAGS_TO_ADD}"    
 
 # set outputs
 
-echo ::set-output name=tags::$TAGS_TO_ADD
-echo ::set-output name=tag_from_git_tag::$TAG_FROM_GIT_TAG
-echo ::set-output name=tag_latest::$TAG_LATEST
-echo ::set-output name=tag_custom::$TAG_CUSTOM
-echo ::set-output name=tag_with_suffix::$TAG_WITH_SUFFIX
+echo ::set-output name=tags::"$TAGS_TO_ADD"
+echo ::set-output name=tag_from_git_tag::"$TAG_FROM_GIT_TAG"
+echo ::set-output name=tag_latest::"$TAG_LATEST"
+echo ::set-output name=tag_custom::"$TAG_CUSTOM"
+echo ::set-output name=tag_with_suffix::"$TAG_WITH_SUFFIX"
